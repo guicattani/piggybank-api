966bb7b4bec33d50d8fe738804ac6d3dc1f377c1
Guilherme Cattani <gcattani1@gmail.com>
2020-02-21 15:53:36 -0300

    Checkpoint

--- app/models/saving_schedule.rb
+++ app/models/saving_schedule.rb
@@ -1,2 +1,15 @@
+# frozen_string_literal: true
+
 class SavingSchedule < ApplicationRecord
+  validates :active, inclusion: { in: [true, false] }
+  validates :description, allow_nil: false
+  validates :period, inclusion: { in: ['day, week, month'] }
+  # validate :value_in_cents
+
+  # t.boolean :active
+  # t.integer :value_in_cents
+  # t.string :description
+  # t.string :period
+  # t.integer :period_value
+
 end
--- db/migrate/20200221173757_create_saving_schedules.rb
+++ db/migrate/20200221173757_create_saving_schedules.rb
@@ -1,12 +1,13 @@
 class CreateSavingSchedules < ActiveRecord::Migration[6.0]
   def change
     create_table :saving_schedules do |t|
-      t.boolean :active
+      t.boolean :active, default: true
       t.integer :value_in_cents
       t.string :description
       t.string :period
       t.integer :period_value
 
+      t.date :next_date
       t.timestamps
     end
   end
